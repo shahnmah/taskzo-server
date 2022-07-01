6abc615d08bf684647534b9b8fbfb196b562c18c
shahnmah <shahn.mah@gmail.com>
2022-07-01 10:07:36 +0600

    task load api added

--- index.js
+++ index.js
@@ -9,19 +9,34 @@ app.use(express.json())
 
 // taskzo_admin dHuoHXX6NsiW76Rd
 
-
-
-
-
-const uri = "mongodb+srv://taskzo_admin:<password>@cluster3.adrhe.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster3.adrhe.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster2.ic3nj.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const taskCollection = client.db('taskCollection').collection('task');
+
+        // get api for load all task
+        app.get('/task', async (req, res) => {
+            const query = {};
+            const cursor = taskCollection.find(query);
+            const tasks = await cursor.toArray();
+            res.send(tasks);
+        });
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send('Taskzo server is running')
-  })
-  app.listen(port, () => {
+})
+app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-  })
+})
