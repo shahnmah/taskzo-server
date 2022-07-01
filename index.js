aed6e6854d4e103cce0e90af1a267d9e71cb873f
shahnmah <shahn.mah@gmail.com>
2022-07-02 01:37:56 +0600

    okay

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-// taskzo_admin dHuoHXX6NsiW76Rd
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster3.adrhe.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -32,8 +30,6 @@ async function run() {
             const newTask = await taskCollection.insertOne(data)
             res.send(newTask)
         });
-
-
 
         // delete product using id
         app.delete('/task/:id', async (req, res) => {
